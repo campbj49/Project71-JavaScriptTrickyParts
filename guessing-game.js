8c779a57123766c2ac92944e7daedc625fe4bf60
John Campbell <campbj49@gmail.com>
2023-09-29 14:49:26 -0400

    guessing game passing tests

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,18 @@
 function guessingGame() {
-
+    let randomNumber = Math.floor(Math.random()*100);
+    let numGuesses = 0;
+    let finished = false;
+    function checkGuess(guess){
+        if(finished) return "The game is over, you already won!";
+        numGuesses++;
+        if(guess=== randomNumber){ 
+            finished = true;
+            return `You win! You found ${randomNumber} in ${numGuesses} guesses.`
+        }
+        if(guess>randomNumber) return `${guess} is too high!`
+        return `${guess} is too low!`;
+    }
+    return checkGuess;
 }
 
 module.exports = { guessingGame };
