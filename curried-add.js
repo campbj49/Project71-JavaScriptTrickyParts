182ea93c2faae2d9ef0d49673dac16575b89d559
John Campbell <campbj49@gmail.com>
2023-09-29 15:51:46 -0400

    finished curried add

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,16 @@
-function curriedAdd(total) {
+function curriedAdd(total=0) {
+    if(total === 0) return 0;
 
+    function add(newNum){
+        if(newNum) {
+            total += newNum;
+            return add;
+        }
+        console.log(newNum);
+        return total;
+    }
+    
+    return add;
 }
 
 module.exports = { curriedAdd };
