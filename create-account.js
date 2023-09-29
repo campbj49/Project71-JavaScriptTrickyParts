71db02017269693c773c7352cb8205637e710b9a
John Campbell <campbj49@gmail.com>
2023-09-29 15:31:54 -0400

    create-account passing the tests

--- create-account.js
+++ create-account.js
@@ -1,5 +1,32 @@
-function createAccount(pin, amount) {
 
+/**creates a bank account object */
+function createAccount(pin, amount=0) {
+    return {
+        checkBalance:(enteredPin)=>{
+        if(enteredPin !== pin) return "Invalid PIN."
+        return `$${amount}`
+        },
+
+        deposit: (enteredPin, depAmount)=>{
+            if(enteredPin !== pin) return "Invalid PIN."
+            amount += depAmount;
+            return `Succesfully deposited $${depAmount}. Current balance: $${amount}.`
+        },
+
+        withdraw:(enteredPin, withAmount)=>{
+            if(enteredPin !== pin) return "Invalid PIN."
+            if(withAmount> amount) return "Withdrawal amount exceeds account balance. Transaction cancelled."
+            amount -= withAmount;
+            return `Succesfully withdrew $${withAmount}. Current balance: $${amount}.`
+
+        },
+        changePin:(enteredPin, newPin)=>{
+            if(enteredPin !== pin) return "Invalid PIN."
+            pin = newPin;
+            return "PIN successfully changed!"
+        }
+
+    }
 }
 
 module.exports = { createAccount };
